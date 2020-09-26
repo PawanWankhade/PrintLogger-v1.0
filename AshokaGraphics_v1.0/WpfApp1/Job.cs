acfda8f29417e16bff0d108b6862f5eb658e156f
Pawan Wankhade <pawanwankhade@hotmail.com>
2020-09-26 17:56:16 +0530

    Initial Commit
    Initial commit in master branch.

---
+++ AshokaGraphics_v1.0/WpfApp1/Job.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AshokaGraphics
+{
+    class Job
+    {
+        public int Id { get; set; }
+        public string JobDate { get; set; }
+        public string FilePath { get; set; }
+        public string PercentComplete { get; set; }
+        public string JobStartTime { get; set; }
+        public string JobEndTime { get; set; }
+        public string TimeRequired { get; set; }
+
+
+
+        public bool CheckFileExists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        private string[] ReadFileContent(string path)
+        {
+            if (File.Exists(path))
+            {
+                string[] fileContent = File.ReadAllLines(path);
+                Array.Reverse(fileContent);
+                return fileContent;
+            }
+            return null;
+        }
+
+        public bool ChangeFileAttribute(string path)
+        {
+            //try
+            //{
+            //    System.Diagnostics.Process process = new System.Diagnostics.Process();
+            //    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            //    startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            //    startInfo.FileName = "cmd.exe";
+            //    startInfo.Arguments = "/C copy /b Image1.jpg + Archive.rar Image2.jpg";
+            //    process.StartInfo = startInfo;
+            //    process.Start();
+            //}
+            //catch (Exception ex)
+            //{
+            //    throw ex;
+            //}
+            return false;
+        }
+
+        public List<Job> GetAllJobs(string path)
+        {
+            try
+            {
+                var FileContent = this.ReadFileContent(path);
+                if (FileContent != null)
+                {
+                    int count = 1;
+                    List<Job> jobs = new List<Job>();
+                    foreach (var item in FileContent)
+                    {
+                        int id = count++;
+                        string[] ar = item.Split(';');
+                        //extract Filepath
+                        string arrElementObj = ar[0];
+                        //string filePath = arrElementObj.Substring(arrElementObj.IndexOf(":\\") - 1, arrElementObj.Length - 10);
+                        string filePath = arrElementObj.Substring(arrElementObj.IndexOf(":\\") - 1);
+                        string jobEndTime = arrElementObj.Substring(0, arrElementObj.IndexOf(":\\") - 2);
+
+
+                        //extract date and time
+                        arrElementObj = ar[1];
+                        string[] temp = arrElementObj.Split('-');
+                        //DateTime jobDate = Convert.ToDateTime(temp[0] + " " + temp[1]);
+                        string jobDate = temp[0];
+                        string jobTime = temp[1];
+
+                        //extract time required 
+                        string timeRequired = ar[2];
+
+                        //extract percent complete
+                        string percentComplete = ar[3];
+
+                        jobs.Add(new Job { Id = id, JobDate = jobDate, JobStartTime = jobTime, JobEndTime = jobEndTime, FilePath = filePath, TimeRequired = timeRequired, PercentComplete = percentComplete });
+
+                    }
+                    return jobs;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+    }
+}
+
---
+++ AshokaGraphics_v1.0/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AshokaGraphics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Job> jobs = null;
+        string filePath = ConfigurationManager.AppSettings.Get("filePath");
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            //this.BindGrid(@"C:\Program Files (x86)\PrinterManager\PrintedArea.Log");
+            this.BindGrid(filePath);
+        }
+        public void BindGrid(string path)
+        {
+            try
+            {
+                Job job = new Job();
+                if (job.CheckFileExists(path))
+                {
+                    jobs = job.GetAllJobs(path);
+                    if (jobs != null || jobs.Count >= 0)
+                    {
+                        DataGrid1.ItemsSource = jobs;
+                        lblStatus.Visibility = Visibility.Hidden;
+                    }
+                    else
+                    {
+                        DataGrid1.Visibility = Visibility.Hidden;
+                        lblStatus.Visibility = Visibility.Visible;
+                        lblStatus.Content = "No data to display";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLogs(ex.Message);
+            }
+
+        }
+        public void CheckFiles(string path)
+        {
+
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (jobs != null)
+                {
+                    DataGrid1.ItemsSource = jobs.FindAll(x => x.JobDate.ToString().Contains(searchTxtBox.Text) || x.FilePath.ToLower().Contains(searchTxtBox.Text.ToLower()) || x.PercentComplete.Contains(searchTxtBox.Text));
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLogs(ex.Message);
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                searchTxtBox.Text = "";
+                if (jobs != null)
+                {
+                    DataGrid1.ItemsSource = jobs;
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLogs(ex.Message);
+            }
+
+        }
+
+        public static void WriteLogs(string message)
+        {
+            File.AppendAllText("logs.txt", DateTime.Now + " " + message + "\n", Encoding.UTF8);
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            this.BindGrid(filePath);
+        }
+    }
+}
